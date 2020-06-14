c9a65784514a4baa7c61f7779b5dce1ba931f8af
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2020-06-14 10:30:31 +0200

    chore: try hello

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,10 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-import greeting from "./../../server/json/greeting.json";
+import { join } from "path";
+const greeting: Object = require(join(
+  __dirname,
+  "_files/json",
+  "greeting.json"
+));
 
 export default (_: NowRequest, res: NowResponse) => {
   res.json(greeting);
